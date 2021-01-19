2af09d22d61849b7e53ac76987eef5f84579efb7
work2544 <work21_123@hotmail.com>
2021-01-19 17:06:20 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,42 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[])
+{
+double temp=0;
+B[4]=0;
+B[5]=100;
+for(int i=0;i<N;i++)
+B[0]+=A[i];
+B[0]/=N;
+for (int i = 0; i < N; i++)
+{
+    temp+=pow(A[i]-B[0],2);
+}
+B[1]=pow(temp/(N),0.5);
+temp=1;
+for (int i = 0; i < N; i++)
+{
+    temp*=A[i];
+}
+B[2]=pow(temp,1.0/N);
+temp=0;
+for (int i = 0; i < N; i++)
+{
+    temp+=1/A[i];
+}
+B[3]=N/temp;
+for (int i = 0; i < N; i++)
+{
+    if(A[i]>B[4])
+    {
+        B[4]=A[i];
+    }
+    else if(A[i]<B[5])
+    {
+        B[5]=A[i];
+    }
+}
+
+
+}
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -13,11 +13,64 @@ int main()
 {
     bool image[N][M] = {};
     int s,x,y;
+    
     do{
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
+       updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M],int s,int x,int y)
+{
+
+for(int i=0;i<N;i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        if(pow(pow(i-x,2)+pow(j-y,2),0.5)<=s-1)
+        {
+            image[i][j]=1;
+        }
+
+    }
+    
+}
+
+
+}
+void showImage(const bool image[][M])
+{
+string picture[32][72];
+for(int i=0;i<32;i++)
+{
+    for (int j = 0; j < 72; j++)
+    {
+        if(i==0||i==31)
+        {
+            picture[i][j]='-';
+        }
+        else if(j==0||j==71)
+        {
+            picture[i][0]="|";
+            picture[i][71]="|";
+        }
+        else
+        {
+            picture[i][j]=" ";
+        }
+    } 
+}
+for(int i=0;i<32;i++)
+{
+    for (int j = 0; j < 72; j++)
+    {
+    if(i<30&&j<70&&image[i][j])
+    {picture[i+1][j+1]='*';}    
+    cout<<picture[i][j];  
+    } 
+    cout<<endl;
+}
+}
+
